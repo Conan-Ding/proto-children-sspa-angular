7a9f5d22260e309d89c5f725431027c99c3fa2de
Conan-Ding <cding@ameripharma.us>
2023-12-11 14:27:55 -0800

    w.i.p:update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { MailInboxComponent } from './pages/inbox/inbox.component';
+import { MailDetailComponent } from './pages/mail-detail/mail-detail.component';
+import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   {
     path: 'inbox',
     component: MailInboxComponent,
   },
-  { path: '', component: EmptyRouteComponent, pathMatch: 'full' },
+  { path: 'details/:id', component: MailDetailComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {path: '**', component: EmptyRouteComponent},
 ];
 
 @NgModule({
-  declarations: [MailInboxComponent],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  declarations: [MailInboxComponent, MailDetailComponent, HomeComponent],
+  imports: [CommonModule , RouterModule.forRoot(routes)],
+  exports: [RouterModule, MailInboxComponent, MailDetailComponent, HomeComponent],
   // use value must match the sub path in the single spa route
   providers: [{ provide: APP_BASE_HREF, useValue: '/app2' }],
 })
