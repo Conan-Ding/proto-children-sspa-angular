17be2d2c008a3c135508057cd9bb6c885faee93d
Conan-Ding <cding@ameripharma.us>
2023-12-13 13:22:24 -0800

    w.i.p: get data from global store

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,6 +1,26 @@
-const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+const singleSpaAngularWebpack =
+  require("single-spa-angular/lib/webpack").default;
+// const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { AngularWebpackPlugin } = require("@ngtools/webpack");
 
 module.exports = (config, options) => {
+  // config.plugins.push(
+  //   new ModuleFederationPlugin({
+  //     name: "app3",
+  //     filename: "remoteEntry.js",
+  //     exposes: {
+  //       "./app3-moduel": "./src/app/app.module.ts",
+  //     },
+  //     remotes: {
+  //       host: "host@http://localhost:9000/remoteEntry.js"
+  //     },
+  //   })
+  // );
+  config.plugins.push(
+    new AngularWebpackPlugin({
+      tsconfig: "./tsconfig.json",
+    })
+  );
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
   // Feel free to modify this webpack config however you'd like to
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { autorun } from 'mobx';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+export class HomeComponent implements OnInit {
+  constructor(private cd: ChangeDetectorRef) {}
+  username = '';
+  store: any;
 
-export class HomeComponent {
-  constructor() {}
+  async ngOnInit() {
+    const remoteModule = await loadRemoteModule({
+      remoteEntry: 'http://localhost:9000/remoteEntry.js',
+      remoteName: 'host',
+      exposedModule: './CentralStore',
+    });
+    this.store = remoteModule.default;
+    this.username = this.store.username;
+
+    autorun(() => {
+      this.username = this.store.username;
+      this.cd.detectChanges(); // Manually trigger Angular's change detection
+    });
+    console.log(this.store);
+    // Now you can use GlobalStore in your Angular application
+  }
+  updateStore() {
+    this.store.setData({
+      username: 'John Doe',
+    });
+  }
 }
